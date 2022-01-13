f4031fa07daa53b8981caeb11e2308e4258ddc29
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-13 23:06:23 +0100

    Added needed routes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,12 +1,12 @@
-<header class="flex w-screen justify-between px-3 py-4 bg-slate-300">
-	<h1 class="text-3xl text-gray-800 font-black">Animal Shelter</h1>
-	<nav>
+<header class="w-screen px-3 py-4 bg-slate-300 shadow-md">
+	<nav class="flex justify-between max-w-7xl mx-auto">
+		<a href="/"><h1 class="text-3xl text-gray-800 font-black">Animal Shelter</h1></a>
 		<ul class="flex justify-around m-2">
-			<li><a href="/">About us</a></li>
-			<li><a href="/">Our pets</a></li>
-			<li><a href="/">Help us</a></li>
-			<li><a href="/">Contact</a></li>
+			<li><a href="/about">About us</a></li>
+			<li><a href="/pets">Our pets</a></li>
+			<li><a href="/help-us">Help us</a></li>
+			<li><a href="/contact">Contact</a></li>
 		</ul>
+		<button>Learn more</button>
 	</nav>
-	<button>Learn more</button>
 </header>
