8e86edd541b8e12d7847bb2ef208ab9f473ae8b1
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-13 23:24:20 +0100

    Navbar: Fixed styling

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,8 @@
-<header class="w-screen px-3 py-4 bg-slate-300 shadow-md">
+<header class="fixed w-full px-10 py-4 bg-slate-300 shadow-md">
 	<nav class="flex justify-between max-w-7xl mx-auto">
-		<a href="/"><h1 class="text-3xl text-gray-800 font-black">Animal Shelter</h1></a>
+		<a href="/">
+			<h1 class="text-3xl text-gray-800 font-black font-serif">Animal Shelter</h1>
+		</a>
 		<ul class="flex justify-around m-2">
 			<li><a href="/about">About us</a></li>
 			<li><a href="/pets">Our pets</a></li>
