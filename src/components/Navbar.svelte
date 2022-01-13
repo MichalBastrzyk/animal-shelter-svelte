b49c244242af9df5965ee6975f3ab31864255647
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-13 22:53:34 +0100

    Added tailwindcss

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,12 @@
-<script>
-    // your script goes here
-</script>
-
-<header>
-    <nav>
-
-    </nav>
+<header class="flex w-screen justify-between px-3 py-4 bg-slate-300">
+	<h1 class="text-3xl text-gray-800 font-black">Animal Shelter</h1>
+	<nav>
+		<ul class="flex justify-around m-2">
+			<li><a href="/">About us</a></li>
+			<li><a href="/">Our pets</a></li>
+			<li><a href="/">Help us</a></li>
+			<li><a href="/">Contact</a></li>
+		</ul>
+	</nav>
+	<button>Learn more</button>
 </header>
