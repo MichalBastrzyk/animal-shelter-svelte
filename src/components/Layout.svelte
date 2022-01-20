053dca7c015d8361945a9e7669e81b157aa18b4f
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-20 18:18:42 +0100

    Layout:  apply margins by using Layout component

---
+++ src/components/Layout.svelte
@@ -0,0 +1,3 @@
+<section class="max-w-5xl m-auto py-20 px-3">
+	<slot><!-- optional fallback --></slot>
+</section>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import '../app.css';
 	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <main class="dark">
 	<Navbar />
-	<div class="max-w-5xl m-auto px-3">
-		<slot />
-	</div>
+	<!-- <div class="min-h-[85vh] max-w-5xl m-auto py-20 px-3"> -->
+	<slot />
+	<!-- </div> -->
+	<Footer />
 </main>
