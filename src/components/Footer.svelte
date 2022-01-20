c030a61025ae576c4c790145e189a09584e456a7
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-20 18:19:14 +0100

    Footer: added

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+	// your script goes here
+</script>
+
+<!-- bg-gradient-to-r from-bg-primary to-black text-white shadow-lg  -->
+
+<footer class="flex justify-center items-center h-56 gradient-bg text-white shadow-lg">
+	<div class="flex flex-col px-3 ">
+		<h2 class="text-2xl font-serif">For questions and suggestions</h2>
+		<span>email@shelter.com</span>
+		<span>+13 674 567 75 54</span>
+	</div>
+	<div class="flex flex-col px-3">
+		<h2 class="text-2xl font-serif">We are waiting for your visit</h2>
+		<span>Boston, Central Street, 1st (Entrance from the store)</span>
+		<span>London, South Park, 18st</span>
+	</div>
+</footer>
