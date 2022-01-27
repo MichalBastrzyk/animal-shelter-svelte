9bdb4b41e3f0d6c52d0c135e3f3b51cf678eb169
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-27 03:01:21 +0100

    Layout: now  can specify bg color thorough parent

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -1,3 +1,9 @@
-<section class="max-w-5xl m-auto py-20 px-3">
-	<slot><!-- optional fallback --></slot>
+<script>
+	export let bgColor = null;
+</script>
+
+<section class={bgColor ?? bgColor}>
+	<div class="max-w-5xl m-auto py-20 px-3">
+		<slot><!-- optional fallback --></slot>
+	</div>
 </section>
