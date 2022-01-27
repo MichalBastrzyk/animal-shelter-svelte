820c4d69b64bbd950d01487a1cd87fb783bb93bc
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-27 02:57:32 +0100

    tailwind.config: added new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ module.exports = {
 	purge: ['./src/**/*.svelte'],
 	content: [],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'bg-gray': '#D9DCD9',
+				'bg-primary': '#332B28',
+				'bg-secondary': '#9A7860',
+				'bg-accent': '#C8A584',
+				'pr-accent': '#FDDCC4',
+			},
+		},
 	},
 	plugins: []
 };
