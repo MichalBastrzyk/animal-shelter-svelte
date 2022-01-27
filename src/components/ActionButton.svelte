5140f5b34d723542bf1cf75dfeb31f8350929626
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-01-27 02:57:12 +0100

    ActionButton: added

---
+++ src/components/ActionButton.svelte
@@ -0,0 +1,14 @@
+<script>
+	// If true changes default background value to transparent and fills itself if hovered over.
+	export let fillOnHover = false;
+</script>
+
+<button
+	class="p-3 px-10 text-black font-semibold font-serif  rounded-full border-2 border-pr-accent  \
+	transition-all duration-300 ease-in-out {fillOnHover
+		? 'bg-transparent hover:bg-pr-accent'
+		: 'bg-pr-accent'}"
+	on:click
+>
+	<slot />
+</button>
