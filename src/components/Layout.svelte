41f3c11be1dc98c03cf1309243e5d4d89061d4e2
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-02-03 17:03:57 +0100

    Layout: Page load animations

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -1,8 +1,10 @@
 <script>
+	import { fly, fade } from 'svelte/transition';
+
 	export let bgColor = null;
 </script>
 
-<section class={bgColor ?? bgColor}>
+<section class={bgColor ?? bgColor} in:fly={{ duration: 300, y: 100 }} out:fade={{ duration: 100 }}>
 	<div class="max-w-5xl m-auto py-20 px-3">
 		<slot><!-- optional fallback --></slot>
 	</div>
