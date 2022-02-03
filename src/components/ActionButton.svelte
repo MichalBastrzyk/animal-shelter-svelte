1546bad89fa29396fb6be0b137a9d57359968eed
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-02-03 17:03:26 +0100

    ActionButton: Style update

--- src/components/ActionButton.svelte
+++ src/components/ActionButton.svelte
@@ -5,7 +5,7 @@
 
 <button
 	class="p-3 px-10 text-black font-semibold font-serif  rounded-full border-2 border-pr-accent  \
-	transition-all duration-300 ease-in-out {fillOnHover
+	transition-all duration-300 ease-in-out hover:shadow-md active:shadow-inner {fillOnHover
 		? 'bg-transparent hover:bg-pr-accent'
 		: 'bg-pr-accent'}"
 	on:click
