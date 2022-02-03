403dbaf9199b5d1c84ca667a853ff77ed4f226ef
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-02-03 17:04:51 +0100

    Card: Extracted styles to other component

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,17 +1,11 @@
 <script>
-	import ActionButton from './ActionButton.svelte';
-
-	export let title, src, alt;
-
-	// export let href;
+	export let Class = null;
 </script>
 
 <div
-	class="flex flex-col justify-around text-center m-2 px-4 py-2 \
+	class={`flex flex-col justify-around text-center m-2 \
 	 bg-white rounded-2xl shadow-sm \
-	 transition-all duration-500 ease-in-out hover:shadow-2xl"
+	 transition-all duration-500 ease-in-out hover:shadow-2xl ${Class ? Class : ''}`}
 >
-	<img {src} {alt} class="lg:w-80  h-full object-cover rounded-2xl" />
-	<h1 class="text-2xl my-8 mx-2">{title}</h1>
-	<ActionButton>Meet me</ActionButton>
+	<slot><!-- optional fallback --></slot>
 </div>
---
+++ src/components/CardSwiperCard.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Card from './Card.svelte';
+	import ActionButton from './ActionButton.svelte';
+
+	export let title, src, alt;
+</script>
+
+<Card>
+	<img {src} {alt} class="lg:w-80 sm:h-auto object-cover rounded-t-2xl" />
+	<div class="px-4 py-2">
+		<h1 class="text-2xl my-8 mx-2">{title}</h1>
+		<ActionButton>Meet me</ActionButton>
+	</div>
+</Card>
