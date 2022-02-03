370fcf02d9fb04427dd521ce2c8345bbc582754c
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-02-03 17:05:49 +0100

    Some trash

---
+++ src/routes/pets.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Layout from '../components/Layout.svelte';
+	import PetBrowser from '../components/PetBrowser.svelte';
+</script>
+
+<Layout>
+	<PetBrowser />
+</Layout>
---
+++ src/routes/pets/[id].svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+
+	let pageStore = $page;
+</script>
+
+<h1>{pageStore.params.id}</h1>
