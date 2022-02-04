03f74acff6f39c602195aca1983c7062620cc57d
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-02-04 18:28:19 +0100

    Created animated hamburger menu: AnimatedHamburger

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 	import NavLink from './NavLink.svelte';
+	import AnimatedHamburger from './SVG/AnimatedHamburger.svelte';
 
 	$: route = $page.url.pathname;
 	// List of routes where adaptive navbar will work in
@@ -9,9 +11,15 @@
 	// Set initial style based on current route
 	$: filledBg = !enabledRoutes.includes(route);
 
+	let open = false;
+
 	onMount(() => {
 		if (enabledRoutes.includes(route)) filledBg = window.scrollY >= window.innerHeight - 200;
 	});
+
+	const closeMenu = () => {
+		if (open) return (open = false);
+	};
 </script>
 
 <svelte:window
@@ -22,21 +30,64 @@
 />
 
 <header
-	class={`fixed w-full px-10 py-4 z-50 transition-all duration-300 ease-in-out ${
+	class={`fixed w-screen px-10 py-4 z-50 transition-all duration-300 ease-in-out ${
 		filledBg ? 'bg-white shadow-xl' : 'text-white'
-	}`}
+	} 
+	${open ? 'shadow-none text-gray-800' : ''}
+	`}
 >
-	<nav class="flex justify-evenly max-w-7xl mx-auto">
-		<a href="/">
-			<h1 class={`text-3xl font-black font-serif ${filledBg ? 'text-gray-800' : 'text-gray-300'}`}>
+	<nav class="flex justify-evenly items-center max-w-7xl mx-auto sm:justify-around">
+		<a href="/" on:click={closeMenu}>
+			<h1
+				class={`flex-1 text-3xl font-black font-serif ${
+					filledBg || open ? 'text-gray-800' : 'text-white'
+				}`}
+			>
 				Animal Shelter
 			</h1>
 		</a>
-		<ul class="flex justify-around m-2">
+		<ul class="hidden justify-around m-2 sm:flex">
 			<li class="mx-2"><NavLink href="/about">About us</NavLink></li>
 			<li class="mx-2"><NavLink href="/pets">Our pets</NavLink></li>
 			<li class="mx-2"><NavLink href="/help-us">Help us</NavLink></li>
 			<li class="mx-2"><NavLink href="/contact">Contact</NavLink></li>
 		</ul>
+		<div class="fixed top-0 right-0 sm:hidden">
+			<AnimatedHamburger bind:open />
+		</div>
 	</nav>
 </header>
+
+{#if open}
+	<div transition:fade class="overlay flex flex-col justify-center items-center text-center">
+		<ul on:click={closeMenu}>
+			<li>
+				<NavLink href="/about">About us</NavLink>
+			</li>
+			<li>
+				<NavLink href="/pets">Our pets</NavLink>
+			</li>
+			<li>
+				<NavLink href="/help-us">Help us</NavLink>
+			</li>
+			<li>
+				<NavLink href="/contact">Contact</NavLink>
+			</li>
+		</ul>
+	</div>
+{/if}
+
+<style>
+	.overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 40;
+
+		width: 100%;
+		height: 100%;
+
+		overflow: hidden;
+		background-color: #fff;
+	}
+</style>
