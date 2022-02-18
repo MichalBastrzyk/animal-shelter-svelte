942bc33ba79cb14cbcc30248c48f3cfcc65518ea
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-02-18 17:24:04 +0100

    --BugFix: Fixed #1

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -1,10 +1,8 @@
 <script>
-	import { fly, fade } from 'svelte/transition';
-
 	export let bgColor = null;
 </script>
 
-<section class={bgColor ?? bgColor} in:fly={{ duration: 300, y: 100 }} out:fade={{ duration: 100 }}>
+<section class={bgColor ?? bgColor}>
 	<div class="max-w-5xl m-auto py-20 px-3">
 		<slot><!-- optional fallback --></slot>
 	</div>
