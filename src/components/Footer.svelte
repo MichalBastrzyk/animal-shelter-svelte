5db5b2ac5a239e07d3b120c75947396d59bc2d15
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-03-01 19:15:24 +0100

    Footer: reworked styles & added icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,18 +1,31 @@
 <script>
-	// your script goes here
+	import TextWithIcon from './TextWithIcon.svelte';
+	import MdMailOutline from 'svelte-icons/md/MdMailOutline.svelte';
+	import FaPhoneAlt from 'svelte-icons/fa/FaPhoneAlt.svelte';
+	import IoIosPin from 'svelte-icons/io/IoIosPin.svelte';
 </script>
 
 <!-- bg-gradient-to-r from-bg-primary to-black text-white shadow-lg  -->
 
-<footer class="flex justify-center items-center h-56 gradient-bg text-white shadow-lg">
+<footer
+	class="flex flex-col p-4 md:flex-row justify-center md:items-center gradient-bg text-white shadow-lg"
+>
 	<div class="flex flex-col px-3 ">
 		<h2 class="text-2xl font-serif">For questions and suggestions</h2>
-		<span>email@shelter.com</span>
-		<span>+13 674 567 75 54</span>
+		<TextWithIcon text="email@shelter.com">
+			<MdMailOutline slot="icon" />
+		</TextWithIcon>
+		<TextWithIcon text="+13 674 567 75 54">
+			<FaPhoneAlt slot="icon" />
+		</TextWithIcon>
 	</div>
 	<div class="flex flex-col px-3">
 		<h2 class="text-2xl font-serif">We are waiting for your visit</h2>
-		<span>Boston, Central Street, 1st (Entrance from the store)</span>
-		<span>London, South Park, 18st</span>
+		<TextWithIcon text="Boston, Central Street, 1st (Entrance from the store)">
+			<IoIosPin slot="icon" />
+		</TextWithIcon>
+		<TextWithIcon text="London, South Park, 18st">
+			<IoIosPin slot="icon" />
+		</TextWithIcon>
 	</div>
 </footer>
---
+++ src/components/TextWithIcon.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let text;
+</script>
+
+<div class="flex items-center m-1 text-bg-accent">
+	<div class="w-8 h-8 mx-2">
+		<slot name="icon" />
+	</div>
+	<span class="font-serif text-md font-light">{text}</span>
+</div>
