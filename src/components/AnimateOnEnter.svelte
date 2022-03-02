d804f5ed9adb916addd22a0bb97bb3ee0a7501e2
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-03-02 18:10:27 +0100

    Added fade in view animation & basic lazy load img

---
+++ src/components/AnimateOnEnter.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { inview } from 'svelte-inview';
+	import { fade } from 'svelte/transition';
+
+	const inviewOptions = {
+		rootMargin: '-50px',
+		unobserveOnEnter: true
+	};
+
+	const transitionOptions = {};
+
+	let isInView;
+</script>
+
+<div use:inview={inviewOptions} on:change={(e) => (isInView = e.detail.inView)}>
+	{#if isInView}
+		<div in:fade={transitionOptions}>
+			<slot />
+		</div>
+	{/if}
+</div>
---
+++ src/components/LazyLoadImg.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { inview } from 'svelte-inview';
+
+	export let src, alt;
+
+	const inviewOptions = {
+		rootMargin: '50px',
+		unobserveOnEnter: true
+	};
+
+	let isInView;
+</script>
+
+<div use:inview={inviewOptions} on:change={(e) => (isInView = e.detail.inView)}>
+	{#if isInView}
+		<img {src} {alt} />
+	{/if}
+</div>
